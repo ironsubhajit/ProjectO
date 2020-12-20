80b6346ca325cdb3aea91b6bdc8e4845892bf007
ironsubhajit <mandalsubhajit655@gmail.com>
2020-12-20 22:54:29 +0530

    Add logout url for accounts app

--- ProjectO/accounts/urls.py
+++ ProjectO/accounts/urls.py
@@ -6,4 +6,5 @@ app_name = 'accounts'
 
 urlpatterns = [
     path('login/', LoginView.as_view(template_name='accounts/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
 ]
