fb0408565529bc4f0cc0ef8c59f82ecdeaa8a0f2
ironsubhajit <mandalsubhajit655@gmail.com>
2021-10-22 00:01:03 +0530

    SignUp view added

--- ProjectO/accounts/views.py
+++ ProjectO/accounts/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views.generic import CreateView
+from django.urls import reverse_lazy
 
-# Create your views here.
+from . import forms as acc_form
+
+
+class SignupView(CreateView):
+    form_class = acc_form.UserCreateForm
+    success_url = reverse_lazy("accounts:login")
+    template_name = "accounts/signup.html"
