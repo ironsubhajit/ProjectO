b16364fdae91d9080ab81aaa8019588fa31c3e72
ironsubhajit <mandalsubhajit655@gmail.com>
2021-10-22 00:06:01 +0530

    Update accounts url patterns

--- ProjectO/accounts/urls.py
+++ ProjectO/accounts/urls.py
@@ -1,10 +1,20 @@
 from django.urls import path
 from django.contrib.auth.views import LoginView, LogoutView
 
+from . import (
+    views as acc_views,
+    forms as acc_form
+)
+
 
 app_name = 'accounts'
 
 urlpatterns = [
-    path('login/', LoginView.as_view(template_name='accounts/login.html'), name='login'),
+    path('signup/', acc_views.SignupView.as_view(), name='signup'),
+    path('login/', LoginView.as_view(
+            template_name='accounts/login.html',
+            authentication_form=acc_form.UserLoginForm
+            ),
+         name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
 ]
