302ef2e3588f462e5326e8d3b92d8bc88f89366d
ironsubhajit <mandalsubhajit655@gmail.com>
2021-10-21 23:57:10 +0530

    Create User Creation form

---
+++ ProjectO/accounts/forms.py
@@ -0,0 +1,38 @@
+from django.contrib.auth import get_user_model
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+
+
+# new user create form
+class UserCreateForm(UserCreationForm):
+
+    class Meta:
+        fields = ["username", "first_name", "last_name", "email", "password1", "password2"]
+        model = get_user_model()
+
+    def __init__(self, *args, **kwargs):
+        super(UserCreateForm, self).__init__(*args, **kwargs)
+
+        self.fields['username'].widget.attrs['class'] = 'form-control bg-white border-left-0 border-md'
+        self.fields['username'].widget.attrs['id'] = 'username'
+        self.fields['username'].widget.attrs['placeholder'] = 'User Name'
+
+        self.fields['first_name'].widget.attrs['class'] = 'form-control bg-white border-left-0 border-md'
+        self.fields['first_name'].widget.attrs['id'] = 'first_name'
+        self.fields['first_name'].widget.attrs['placeholder'] = 'First Name'
+
+        self.fields['last_name'].widget.attrs['class'] = 'form-control bg-white border-left-0 border-md'
+        self.fields['last_name'].widget.attrs['id'] = 'last_name'
+        self.fields['last_name'].widget.attrs['placeholder'] = 'Last Name'
+
+        self.fields['email'].widget.attrs['class'] = 'form-control bg-white border-left-0 border-md'
+        self.fields['email'].widget.attrs['id'] = 'email'
+        self.fields['email'].widget.attrs['placeholder'] = 'Email Address'
+
+        self.fields['password1'].widget.attrs['class'] = 'form-control bg-white border-left-0 border-md'
+        self.fields['password1'].widget.attrs['id'] = 'password'
+        self.fields['password1'].widget.attrs['placeholder'] = 'Password'
+
+        self.fields['password2'].widget.attrs['class'] = 'form-control bg-white border-left-0 border-md'
+        self.fields['password2'].widget.attrs['id'] = 'passwordConfirmation'
+        self.fields['password2'].widget.attrs['placeholder'] = 'Confirm Password'
+
