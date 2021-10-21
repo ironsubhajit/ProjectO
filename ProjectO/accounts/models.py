86900336b8b6d428afacdfc79e4aa3cbfc47af72
ironsubhajit <mandalsubhajit655@gmail.com>
2021-10-21 23:51:15 +0530

    Override User __str__ method

--- ProjectO/accounts/models.py
+++ ProjectO/accounts/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth import models
 
-# Create your models here.
+
+# user model
+class User(models.User, models.PermissionsMixin):
+
+    def __str__(self):
+        return f"@{self.username}"
