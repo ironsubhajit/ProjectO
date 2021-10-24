7026c77b183384162f827d64b14fcd10ac1c89b9
ironsubhajit <mandalsubhajit655@gmail.com>
2021-10-25 00:36:20 +0530

    Provide user model context to index page

--- ProjectO/ProjectO/views.py
+++ ProjectO/ProjectO/views.py
@@ -1,5 +1,8 @@
 from django.views.generic import TemplateView
+from django.contrib.auth import get_user_model
 
 
 class HomePage(TemplateView):
+    model = get_user_model()
+    context_object_name = 'user'
     template_name = 'ProjectO/index.html'
