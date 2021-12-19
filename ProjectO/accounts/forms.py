04ba58125cad40fef571c82a5c90a928ebe9c28a
ironsubhajit <mandalsubhajit655@gmail.com>
2021-12-19 22:11:16 +0530

    Import built-in forms and User model

--- ProjectO/accounts/forms.py
+++ ProjectO/accounts/forms.py
@@ -1,3 +1,5 @@
+from django import forms
+from django.contrib.auth.models import User
 from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 
