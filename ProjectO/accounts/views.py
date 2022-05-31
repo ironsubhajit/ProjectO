32d94ab6508d117e5cfe64f8c430a01e098718b5
ironsubhajit <mandalsubhajit655@gmail.com>
2022-05-31 12:26:52 +0530

    profile view added

--- ProjectO/accounts/views.py
+++ ProjectO/accounts/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 from django.views.generic import CreateView
 from django.urls import reverse_lazy
@@ -9,3 +10,9 @@ class SignupView(CreateView):
     form_class = acc_form.UserCreateForm
     success_url = reverse_lazy("accounts:login")
     template_name = "accounts/signup.html"
+
+
+@login_required
+def profile(request):
+    """user profile page view"""
+    return render(request, 'accounts/user_profile.html')
