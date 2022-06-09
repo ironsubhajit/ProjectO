b40a17d98f4a48f1ed9063c7302e21f96cd288e9
ironsubhajit <mandalsubhajit655@gmail.com>
2022-06-09 12:07:28 +0530

    Modify save method to save profile image in small sizes

--- ProjectO/accounts/models.py
+++ ProjectO/accounts/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth import models as auth_models
+from PIL import Image
 
 
 # user model
@@ -14,6 +15,17 @@ class Profile(models.Model):
     profile_image = models.ImageField(default="default.jpg", upload_to='profile_pics')
     profile_description = models.CharField(blank=True, null=True, max_length=500)
     collage_name = models.CharField(blank=True, null=True, max_length=200)
+
+    def save(self):
+        """ Change and save profile image size """
+        super().save()
+
+        image = Image.open(self.profile_image.path)
+
+        if image.height > 300 or image.width > 300:
+            output_size = (300, 300)
+            image.thumbnail(output_size)
+            image.save(self.profile_image.path)
 
     def __str__(self):
         return f"{self.user} Profile"
