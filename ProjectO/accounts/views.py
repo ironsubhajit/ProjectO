bdbbfa66f93104cc9deeb7bef50956a0b278cce2
ironsubhajit <mandalsubhajit655@gmail.com>
2022-06-09 12:08:42 +0530

    Profile Update view added

--- ProjectO/accounts/views.py
+++ ProjectO/accounts/views.py
@@ -16,3 +16,41 @@ class SignupView(CreateView):
 def profile(request):
     """user profile page view"""
     return render(request, 'accounts/user_profile.html')
+
+
+@login_required
+def profile_update(request):
+    """user profile update page view"""
+    if request.method == 'POST':
+        user_update_form = acc_form.UserUpdateForm(request.POST, instance=request.user)
+        profile_update_form = acc_form.ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile)
+
+        # print(f"user_update_form.is_valid() ========================================> {user_update_form.is_valid()}")
+        # print(f"user_update_form.is_bound() ========================================> {user_update_form.is_bound}")
+        # print(f"user_update_form.errors() ========================================> {user_update_form.errors}")
+
+        print(f"profile_update_form.is_valid() ========================================> {profile_update_form.is_valid()}")
+
+        if user_update_form.is_valid() and profile_update_form.is_valid():
+            # print(f"========================================> Inside valid")
+
+            user_update_form.save()
+
+            # print(f"========================================> user update validated")
+
+            profile_update_form.save()
+
+            # print(f"========================================> profile update validated")
+
+            return redirect('accounts:profile')
+    else:
+        user_update_form = acc_form.UserUpdateForm(instance=request.user)
+        profile_update_form = acc_form.ProfileUpdateForm(instance=request.user.profile)
+
+    context = {
+        'user_update_form': user_update_form,
+        'profile_update_form': profile_update_form
+    }
+
+    return render(request, 'accounts/user_update_page.html', context=context)
+
