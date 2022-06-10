0812bae2462d99df73cfeb412df46e35cfa34663
ironsubhajit <mandalsubhajit655@gmail.com>
2022-06-10 14:35:08 +0530

    Email reset password functionality added

--- ProjectO/ProjectO/settings.py
+++ ProjectO/ProjectO/settings.py
@@ -135,3 +135,11 @@ MEDIA_ROOT = Path(BASE_DIR, 'media')
 
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
--- ProjectO/accounts/urls.py
+++ ProjectO/accounts/urls.py
@@ -1,5 +1,9 @@
-from django.urls import path
-from django.contrib.auth.views import LoginView, LogoutView
+from django.urls import path, reverse_lazy
+from django.contrib.auth.views import (
+    LoginView, LogoutView,
+    PasswordResetView, PasswordResetConfirmView,
+    PasswordResetDoneView, PasswordResetCompleteView
+)
 
 from . import (
     views as acc_views,
@@ -11,12 +15,44 @@ app_name = 'accounts'
 
 urlpatterns = [
     path('signup/', acc_views.SignupView.as_view(), name='signup'),
-    path('login/', LoginView.as_view(
+    path(
+        'login/',
+        LoginView.as_view(
             template_name='accounts/login.html',
             authentication_form=acc_form.UserLoginForm
-            ),
-         name='login'),
+        ),
+        name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
+    path(
+        'password-reset/',
+        PasswordResetView.as_view(
+            template_name='accounts/password_reset.html',
+            success_url=reverse_lazy('accounts:password_reset_done'),
+            email_template_name='accounts/password_reset_email.html'
+        ),
+        name="password_reset",
+
+    ),
+    path(
+        'password-reset/done',
+        PasswordResetDoneView.as_view(
+            template_name='accounts/password_reset_done.html'),
+        name="password_reset_done"
+    ),
+    path(
+        'password-reset-confirm/<uidb64>/<token>/',
+        PasswordResetConfirmView.as_view(
+            template_name='accounts/password_reset_confirm.html',
+            success_url=reverse_lazy('accounts:password_reset_complete')
+        ),
+        name="password_reset_confirm"
+    ),
+    path(
+        'password-reset-complete/',
+        PasswordResetCompleteView.as_view(
+            template_name='accounts/password_reset_complete.html'),
+        name='password_reset_complete'
+    ),
     path('profile/', acc_views.profile, name="profile"),
     path('update/profile/', acc_views.profile_update, name="update_profile"),
 ]
